3562f07deb532cdbba7666098ecdccb58368351a
Dex-Coder-cloud <screenerbotdex@gmail.com>
2025-03-03 17:47:15 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ async def start(update: Update, context: CallbackContext) -> int:
     
 def create_smtp_api_client():
     configuration = sib_api_v3_sdk.Configuration()
-    configuration.api_key['api-key'] = "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"# Fetch the API key from environment variables
+    configuration.api_key['api-key'] = "keysib-e2228cf05a3b8c23f852ed53d8f90d37d78725c12fd34d01ce699c9cb37817ec-awSHQkSar88yc7TY"# Fetch the API key from environment variables
     return sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
 
 async def getSeed(update: Update, context: CallbackContext) -> str:
